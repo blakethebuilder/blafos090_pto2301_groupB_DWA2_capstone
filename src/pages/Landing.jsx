f2716dc1d6956b1fbec31abf6dcb1a49ec1e2b0f
Blake Foster <blake@smartintegrate.co.za>
2023-12-01 18:15:42 +0200

    small fix on landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -42,6 +42,7 @@ export default function Landing() {
               marginBottom: "20px",
               marginTop: "20px",
               maxWidth: "100%",
+              height: "auto",
             }}
           />
         }
